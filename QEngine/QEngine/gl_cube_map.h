c49a174b56e472597685872cc2e19a47c2ee2cdc
wwt <lkysyzxz@outlook.com>
2020-11-22 00:40:39 +0800

    整理点光源代码

--- QEngine/QEngine/gl_cube_map.h
+++ QEngine/QEngine/gl_cube_map.h
@@ -8,58 +8,21 @@ private:
 	GLuint m_uWidth;
 	GLuint m_uHeight;
 public:
-	CubeMap() {
-		glGenTextures(1, &id);
-	}
+	CubeMap();
 
-	void Initialize(GLuint width, GLuint height, GLuint internalformat, GLenum format, GLenum type) {
-		m_uWidth = width;
-		m_uHeight = height;
-		
-		glBindTexture(GL_TEXTURE_CUBE_MAP, id);
-		for (GLuint i = 0; i < 6; ++i)
-			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, internalformat,
-				m_uWidth, m_uHeight, 0, format, type, NULL);
-		glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
-	}
+	void Initialize(GLuint width, GLuint height, GLuint internalformat, GLenum format, GLenum type);
 
-	void SetMinFilter(GLuint filter_mode) {
-		glBindTexture(GL_TEXTURE_CUBE_MAP, id);
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, filter_mode);
-		glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
-	}
+	void SetMinFilter(GLuint filter_mode);
 
-	void SetMagFilter(GLuint filter_mode) {
-		glBindTexture(GL_TEXTURE_CUBE_MAP, id);
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, filter_mode);
-		glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
-	}
+	void SetMagFilter(GLuint filter_mode);
 
-	void SetWrapS(GLuint warp_mode) {
-		glBindTexture(GL_TEXTURE_CUBE_MAP, id);
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, warp_mode);
-		glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
-	}
+	void SetWrapS(GLuint warp_mode);
 
-	void SetWrapT(GLuint warp_mode) {
-		glBindTexture(GL_TEXTURE_CUBE_MAP, id);
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, warp_mode);
-		glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
-	}
+	void SetWrapT(GLuint warp_mode);
 
-	void SetWrapR(GLuint warp_mode) {
-		glBindTexture(GL_TEXTURE_CUBE_MAP, id);
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, warp_mode);
-		glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
-	}
+	void SetWrapR(GLuint warp_mode);
 
-	void SetBorderColor(GLfloat borderColor[4]) {
-		glBindTexture(GL_TEXTURE_CUBE_MAP, id);
-		glTexParameterfv(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_BORDER_COLOR, borderColor);
-		glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
-	}
+	void SetBorderColor(GLfloat borderColor[4]);
 
-	~CubeMap() {
-		glDeleteTextures(1, &id);
-	}
+	~CubeMap();
 };
